befa67b250abde93ca743facc3c2eb97dab1dbc3
Jairo Filho <jairofilho79@gmail.com>
2023-04-20 13:32:19 -0300

    MVP event handlers

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello, world!")
+if(document.readyState !== 'complete') { // Verifica se o DOM já está preparado para ser manipulado
+  addEventListener('load', () => { // espera ficar pronto
+    startCreateListeners() 
+  })
+} else { // já está pronto
+  startCreateListeners() 
+}
+
+const startCreateListeners = () => { // foi necessário criar uma função para não colocar esse evento na tag body (apagaria tudo de uma vez)
+  for (let e of document.querySelector('body').children) { // recursivamente seleciona cada elemento (e seus filhos)
+    handleListeners(e) 
+  }
+}
+
+const handleListeners = (element) => { // manipula cada elemento e chama seus filhos
+  element.onmouseenter = (e) => { //início do hover
+    console.log('entrou mouse', e)
+    e.preventDefault() // evita que o botão execute seu comportamento (ex: ir para outra tela)
+    e.stopPropagation() // evita que o pai também seja afetado
+  }
+
+  for (let e of element.children) { // chama os filhos dos elementos
+    handleListeners(e)
+  }
+}
