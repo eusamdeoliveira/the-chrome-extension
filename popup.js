3b882083501bed2ee9afea551f7611715b59fd33
Jairo Filho <jairofilho79@gmail.com>
2023-05-02 09:24:49 -0300

    adding storage

--- popup.js
+++ popup.js
@@ -21,17 +21,34 @@ function sendMessage(key, value) {
   );
 }
 
+function colocaNoStorage(key, value) {
+  chrome.storage.local.set({ [key]: value }).then(() => {
+    console.log("Value is set to " + value);
+  });
+}
+
+function pegaNoStorage(key) {
+  chrome.storage.local.get([key]).then((result) => {
+    console.log("Value currently is " + result.key);
+  });
+}
+
 function colorPick (nome, value) {
   sendMessage(nome,value)
 }
 
 function startDOMEvent() {
   let checkbox = document.querySelector("#ativação")
+  // pegar o valor no storage (active)
   checkbox.addEventListener('click', function (event) {
     sendMessage('active', event.target.checked)
+    //salvar o valor no storage (active)
+
   })
   let colorPicker = document.querySelector("#colorpicker")
+  // pegar o valor no storage (color)
   colorPicker.addEventListener('change', function (event) {
     colorPick('color', event.target.value)
+    //salvar o valor no storage (color)
   })
 }
